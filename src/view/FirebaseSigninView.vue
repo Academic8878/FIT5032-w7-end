2f3e3a18fca4649d900a11949e9f5f84bd137144
academic8878 <academic8878@gmail.com>
2025-09-19 20:10:47 +1000

    add some views

---
+++ src/view/FirebaseSigninView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="firebase-signin">
+    <h1>Firebase Sign In</h1>
+    <p>This is the Firebase authentication page</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FirebaseSigninView'
+}
+</script>
+
+<style scoped>
+.firebase-signin {
+  padding: 20px;
+  text-align: center;
+}
+</style>
